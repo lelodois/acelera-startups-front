c5f767325d933cdea101307945c751c33ba14ab8
Léo José da Costa <leoeduar@gmail.com>
2018-09-16 01:37:36 -0300

    Update url-util.service.ts

--- src/app/provider/url-util.service.ts
+++ src/app/provider/url-util.service.ts
@@ -1,2 +1,2 @@
-export const URL_CUST_EMP = 'http://142.93.125.178:9099/api/customers/company/';
-export const URL_CUST_STA = 'http://142.93.125.178:9099/api/customers/startup/';
+export const URL_CUST_EMP = 'http://54.234.183.203:9099/api/customers/company/';
+export const URL_CUST_STA = 'http://54.234.183.203:9099/api/customers/startup/';
